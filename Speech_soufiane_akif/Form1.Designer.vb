e923f6a30acbd3f9b289890484263bd8f5c012f4
Soufiane Akif <75849464+Soufianeakif@users.noreply.github.com>
2022-03-06 03:00:45 +0100

    Add files via upload

---
+++ Speech_soufiane_akif/Form1.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.GunaElipse1 = New Guna.UI.WinForms.GunaElipse(Me.components)
+        Me.GunaControlBox1 = New Guna.UI.WinForms.GunaControlBox()
+        Me.GunaPanel1 = New Guna.UI.WinForms.GunaPanel()
+        Me.GunaControlBox2 = New Guna.UI.WinForms.GunaControlBox()
+        Me.lbl1 = New Guna.UI.WinForms.GunaLabel()
+        Me.GunaPanel2 = New Guna.UI.WinForms.GunaPanel()
+        Me.GunaPanel1.SuspendLayout()
+        Me.GunaPanel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GunaElipse1
+        '
+        Me.GunaElipse1.TargetControl = Me
+        '
+        'GunaControlBox1
+        '
+        Me.GunaControlBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GunaControlBox1.AnimationHoverSpeed = 0.07!
+        Me.GunaControlBox1.AnimationSpeed = 0.03!
+        Me.GunaControlBox1.IconColor = System.Drawing.Color.Black
+        Me.GunaControlBox1.IconSize = 15.0!
+        Me.GunaControlBox1.Location = New System.Drawing.Point(336, 0)
+        Me.GunaControlBox1.Name = "GunaControlBox1"
+        Me.GunaControlBox1.OnHoverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.GunaControlBox1.OnHoverIconColor = System.Drawing.Color.White
+        Me.GunaControlBox1.OnPressedColor = System.Drawing.Color.Black
+        Me.GunaControlBox1.Size = New System.Drawing.Size(45, 29)
+        Me.GunaControlBox1.TabIndex = 0
+        '
+        'GunaPanel1
+        '
+        Me.GunaPanel1.Controls.Add(Me.GunaControlBox2)
+        Me.GunaPanel1.Controls.Add(Me.GunaControlBox1)
+        Me.GunaPanel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.GunaPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.GunaPanel1.Name = "GunaPanel1"
+        Me.GunaPanel1.Size = New System.Drawing.Size(381, 32)
+        Me.GunaPanel1.TabIndex = 1
+        '
+        'GunaControlBox2
+        '
+        Me.GunaControlBox2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GunaControlBox2.AnimationHoverSpeed = 0.07!
+        Me.GunaControlBox2.AnimationSpeed = 0.03!
+        Me.GunaControlBox2.ControlBoxType = Guna.UI.WinForms.FormControlBoxType.MinimizeBox
+        Me.GunaControlBox2.IconColor = System.Drawing.Color.Black
+        Me.GunaControlBox2.IconSize = 15.0!
+        Me.GunaControlBox2.Location = New System.Drawing.Point(288, 0)
+        Me.GunaControlBox2.Name = "GunaControlBox2"
+        Me.GunaControlBox2.OnHoverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.GunaControlBox2.OnHoverIconColor = System.Drawing.Color.White
+        Me.GunaControlBox2.OnPressedColor = System.Drawing.Color.Black
+        Me.GunaControlBox2.Size = New System.Drawing.Size(45, 29)
+        Me.GunaControlBox2.TabIndex = 1
+        '
+        'lbl1
+        '
+        Me.lbl1.AutoSize = True
+        Me.lbl1.Font = New System.Drawing.Font("Bebas Neue", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl1.Location = New System.Drawing.Point(106, 15)
+        Me.lbl1.Name = "lbl1"
+        Me.lbl1.Size = New System.Drawing.Size(184, 44)
+        Me.lbl1.TabIndex = 2
+        Me.lbl1.Text = "Soufiane akif"
+        '
+        'GunaPanel2
+        '
+        Me.GunaPanel2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.GunaPanel2.Controls.Add(Me.lbl1)
+        Me.GunaPanel2.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.GunaPanel2.Location = New System.Drawing.Point(0, 523)
+        Me.GunaPanel2.Name = "GunaPanel2"
+        Me.GunaPanel2.Size = New System.Drawing.Size(381, 77)
+        Me.GunaPanel2.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(381, 600)
+        Me.Controls.Add(Me.GunaPanel2)
+        Me.Controls.Add(Me.GunaPanel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.GunaPanel1.ResumeLayout(False)
+        Me.GunaPanel2.ResumeLayout(False)
+        Me.GunaPanel2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GunaElipse1 As Guna.UI.WinForms.GunaElipse
+    Friend WithEvents GunaPanel1 As Guna.UI.WinForms.GunaPanel
+    Friend WithEvents GunaControlBox1 As Guna.UI.WinForms.GunaControlBox
+    Friend WithEvents GunaControlBox2 As Guna.UI.WinForms.GunaControlBox
+    Friend WithEvents lbl1 As Guna.UI.WinForms.GunaLabel
+    Friend WithEvents GunaPanel2 As Guna.UI.WinForms.GunaPanel
+
+End Class
---
+++ Speech_soufiane_akif/Form1.vb
@@ -0,0 +1,78 @@
+﻿Imports System.Speech
+'Follow me on tiktok and instagram @soufiane___akif ;)
+'TIKTOK :
+'https://www.tiktok.com/@soufiane__akif
+'INSTAGRAM :
+'https://www.instagram.com/soufiane__akif
+Public Class Form1
+
+    Dim WithEvents Myvoice As New Recognition.SpeechRecognitionEngine
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        MyVoice.SetInputToDefaultAudioDevice()
+
+
+        Dim MyGrammar As New Recognition.SrgsGrammar.SrgsDocument
+
+
+        Dim MyWordsRule As New Recognition.SrgsGrammar.SrgsRule("words")
+
+
+        Dim MyWords As New Recognition.SrgsGrammar.SrgsOneOf("zwina", "Salut", "spotify", "heure")
+
+
+        MyWordsRule.Add(MyWords)
+
+
+        MyGrammar.Rules.Add(MyWordsRule)
+
+        MyGrammar.Root = MyWordsRule
+
+
+        MyVoice.LoadGrammar(New Recognition.Grammar(MyGrammar))
+
+        MyVoice.RecognizeAsync()
+
+    End Sub
+
+    Private Sub reco_RecognizeCompleted(ByVal sender As Object, ByVal e As System.Speech.Recognition.RecognizeCompletedEventArgs) Handles Myvoice.RecognizeCompleted
+
+        Myvoice.RecognizeAsync()
+
+
+
+
+    End Sub
+    Dim SAPI
+
+    Private Sub reco_SpeechRecognized(ByVal sender As Object, ByVal e As System.Speech.Recognition.RecognitionEventArgs) Handles Myvoice.SpeechRecognized
+        SAPI = CreateObject("SAPI.spvoice")
+
+        Select Case e.Result.Text
+            'if the user speaks "simo"
+            Case "zwina"
+                SAPI.Speak("Oui monsieur")
+                lbl1.Text = "Oui monsieur"
+
+            Case "Salut"
+                SAPI.Speak("Salut soufiane")
+                lbl1.Text = "Salut soufiane"
+
+            Case "spotify"
+                'open spotify
+                SAPI.Speak("oui bien sur")
+                Shell("spotify")
+                lbl1.Text = "Enjoy sir ;)"
+
+            Case "heure"
+                'l'heure
+                SAPI.Speak("L'heure est " + Date.Now.ToString("hh:mm"))
+                lbl1.Text = "Heure est " + Date.Now.ToString("hh:mm")
+
+
+        End Select
+
+    End Sub
+
+End Class
